2cf217caaf4eb7dc1e55d413bfb3444f5b58841d
Muhammad Faiz Al Ghozi <muhammadfaizalghozi1@gmail.com>
2021-11-25 16:15:43 +0700

    matnap

---
+++ js/landingPage.js
@@ -0,0 +1,6 @@
+let login = document.getElementById("login")
+let signIn = document.getElementById("signIn")
+
+signIn.addEventListener("click",()=>{
+    login.classList.toggle("login-disappear")
+})
